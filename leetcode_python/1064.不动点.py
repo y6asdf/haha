8c5e924d65329d12fbfa217a5888fe9e41491dfb
y6asdf <1615930226@qq.com>
2021-04-15 21:41:52 +0800

    second

---
+++ leetcode_python/1064.不动点.py
@@ -0,0 +1,20 @@
+def fixedPoint(A):
+    i = 0
+    while i < len(A):
+        if i==A[i]:
+            return i
+        i+=1
+    else:
+        return -1
+
+print(fixedPoint([-10,-5,0,3,7]))
+
+def fixedPoint2(A):
+    for i in range(len(A)):
+        if A[i]==i:
+            return i
+    return -1
+
+print(fixedPoint2([-10,-5,0,3,7]))
+print(fixedPoint2([0,1,2,3,7]))
+import pandas as pd 
